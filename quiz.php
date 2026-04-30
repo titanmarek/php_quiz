3eb0eda1819e0fa295a53ddbba884a4d83800829
titanmarek <kosikm@gchd.cz>
2026-04-30 14:04:41 +0200

    update

--- quiz.php
+++ quiz.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <head>
     <title>Sovy</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
@@ -20,23 +21,23 @@ if (isset($_GET["otazka3"]) && $_GET["otazka3"] == "Ano") {
 if (isset($_GET["otazka1"]) || isset($_GET["otazka2"]) || isset($_GET["otazka3"])) {
     if ($body == 0){
         echo "<h1> Odpověděl jsi na všechno špatně. Tady máš obrázek.</h1>";
-        echo "<img src='angry_owl.jpg' style='width:200px'>";
+        echo "<img src='angry_owl.jpg' class='center-img'  style='border:2px solid black'>";
         echo "<style>body { background-color: red; }</style>";
 
     }
     else if ($body == 1){
          echo "<h1> Odpověděl jsi na 1 ze 3 otázek dobře. Tady máš obrázek.</h1>";
-         echo "<img src='owl1.jpg'style='width:200px'>";
+         echo "<img src='owl1.jpg' class='center-img'  style='border:2px solid black'>";
          echo "<style>body { background-color: orange; }</style>";
     }
     else if ($body == 2){
          echo "<h1> Odpověděl jsi na 2 ze 3 otázek dobře. Tady máš obrázek.</h1>";
-         echo "<img src='owl2.jpg'style='width:200px'>";
+         echo "<img src='owl2.jpg' class='center-img'  style='border:2px solid black'>";
          echo "<style>body { background-color: yellow; }</style>";
     }
     else if ($body == 3){
          echo "<h1> Odpověděl jsi na 3 ze 3 otázek dobře. Tady máš obrázek.</h1>";
-         echo "<img src='happy_owl.jpg'style='width:200px'>";
+         echo "<img src='happy_owl.jpg' class='center-img'  style='border:2px solid black'>";
          echo "<style>body { background-color: green; }</style>";
     }
 }
@@ -51,27 +52,30 @@ else{
 
 
 <form>
-    <label>Mají sovy velký, silný zobák?</label>
-    <input type="text" name="otazka1">
+    <div class="box">
+        <div class="text">
+            <label>Mají sovy velký, silný zobák?</label>
+            <select name="otazka1">
+                <option>Ano</option>
+                <option>Ne</option>
+            </select>
+            <br>
+            <label>Sovy mají špatně vyvinutý sluch?</label>
+            <select name="otazka2">
+                <option>Ano</option>
+                <option>Ne</option>
+            </select>
+            <br>
+            <label>Mají sovy na nohách vždy 4 prsty?</label>
+            <select name="otazka3">
+                <option>Ano</option>
+                <option>Ne</option>
+            </select>
+            <br>
+        </div>
+        <button class='button'><p><strong>Odeslat</strong></p></button>
 
-
-    <br>
-    
-
-
-    <label>Sovy mají špatně vyvinutý sluch?</label>
-    <input type="text" name="otazka2">
-
-
-    <br>
-
-
-    <label>Mají sovy na nohách vždy 4 prsty?</label>
-    <input type="text" name="otazka3">
-
-
-    <br>
-    <button>Odeslat</button>
+    </div>
 </form>
 </body>
 </html>
